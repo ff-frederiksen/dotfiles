7b8f2aab8855c04f2683fe9ca675e02984bc34a4
fred <frederikzen96@gmail.com>
2026-06-13 17:23:19 +0200

    feat: lua LSP & Formatting

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,1 +1,40 @@
-return {}
+return {
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"mason-org/mason.nvim",
+			"mason-org/mason-lspconfig.nvim",
+		},
+		config = function()
+			require("mason").setup()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls" },
+				automatic_enable = false,
+			})
+			vim.lsp.config("lua_ls", {
+				settings = {
+					Lua = {
+						runtime = { version = "LuaJIT" },
+						diagnostics = { globals = { "vim" } },
+						telemetry = { enable = false },
+					},
+				},
+			})
+
+			vim.lsp.enable("lua_ls")
+
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(ev)
+					local map = function(k, fn, d)
+						vim.keymap.set("n", k, fn, { buffer = ev.buf, desc = d })
+					end
+					map("gd", vim.lsp.buf.definition, "Definition")
+					map("gr", vim.lsp.buf.references, "References")
+					map("K", vim.lsp.buf.hover, "Hover")
+					map("<leader>rn", vim.lsp.buf.rename, "Rename")
+					map("<leader>ca", vim.lsp.buf.code_action, "Code action")
+				end,
+			})
+		end,
+	},
+}
--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,16 +1,30 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    cmd = "Telescope",
-    keys = {
-        { "<leader>ff", "<cmd>Telescope find_files<cr>",
-          desc = "Find files" },
-        { "<leader>fg", "<cmd>Telescope live_grep<cr>",
-          desc = "Live grep" },
-        { "<leader>fb", "<cmd>Telescope buffers<cr>",
-          desc = "Buffers" },
-        { "<leader>fh", "<cmd>Telescope help_tags<cr>",
-          desc = "Help tags" },
-    },
-    dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {},
+	"nvim-telescope/telescope.nvim",
+	cmd = "Telescope",
+	keys = {
+		{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
+		{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
+		{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+		{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help tags" },
+		{
+			"<leader>fc",
+			function()
+				require("telescope.builtin").find_files({
+					cwd = vim.fn.stdpath("config"),
+				})
+			end,
+			desc = "Find config files",
+		},
+		{
+			"<leader>fC",
+			function()
+				require("telescope.builtin").live_grep({
+					cwd = vim.fn.stdpath("config"),
+				})
+			end,
+			desc = "Grep config files",
+		},
+	},
+	dependencies = { "nvim-lua/plenary.nvim" },
+	opts = {},
 }
