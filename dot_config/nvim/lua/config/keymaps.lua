0bd5bc7d75ba694879a1ba424d6589726706daae
fred <frederikzen96@gmail.com>
2026-06-18 22:48:34 +0200

    fix: add diagnostic keymap

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -2,8 +2,7 @@ local map = vim.keymap.set
 
 -- Clear search highlight
 map("n", "<leader>nh", "<CMD>nohlsearch<CR>", { desc = "Clear highlights" })
-
+map("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show line diagnostics" })
 -- PLUGINS --
 -- OIL
 map("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-
